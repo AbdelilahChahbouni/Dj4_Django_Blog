2538c7625d9f160d07846d43d9724ec584325b41
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-07-08 19:15:43 +0100

    Create Django Project And Blog App And Models

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=4000)
+    create_date = models.DateTimeField(default=timezone.now())
+    draft = models.BooleanField(default= True)
+
+
+        
+
